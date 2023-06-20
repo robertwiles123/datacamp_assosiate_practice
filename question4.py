0524269b0c70f2e0ede0bb6bbb49b16047f39207
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-06-20 10:49:57 +0000

    Draft answer to Q4

--- question4.py
+++ question4.py
@@ -2,5 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # Reading file to dataframe
 coffee = pd.read_csv('coffee_clean.csv')
+
+# make graph prettier
+# add labeled title etc.
+sns.lineplot(x='Rating', y='Reviews', data=coffee)
+plt.minorticks_on()
+plt.grid(which='both', linewidth=0.5, alpha=0.5, linestyle='--')
+plt.savefig('question4_visual.png', format='png')
