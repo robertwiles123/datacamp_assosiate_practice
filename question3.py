c9ce4f405dcd9f489d7b3765d06c5b63f4c5fbaa
robertwiles123 <133101861+robertwiles123@users.noreply.github.com>
2023-06-20 09:13:59 +0000

    Question 3 draft completed

--- question3.py
+++ question3.py
@@ -6,4 +6,12 @@ import seaborn as sns
 coffee = pd.read_csv('coffee_clean.csv')
 
 # to count how many stores go each potential rating
-stores_grouped_rating = coffee.groupby('Rating')['Place name'].count()
+stores_grouped_rating = coffee.value_counts('Rating')
+
+print(stores_grouped_rating)
+
+sns.set_palette("colorblind")
+sns.histplot(stores_grouped_rating.index, kde=True)
+plt.title('Distribution of resturant ratings', y=1.1)
+plt.xlabel('Ratings')
+plt.savefig('question3_visual.png', format='png')
